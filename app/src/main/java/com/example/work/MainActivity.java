5f3bf0b9649686a0ed194dafb80115c4fdc5f761
dadahe <8791923+dadahe@user.noreply.gitee.com>
2022-09-28 19:12:44 +0800

    add files

--- app/src/main/java/com/example/work/MainActivity.java
+++ app/src/main/java/com/example/work/MainActivity.java
@@ -1,24 +1,24 @@
 package com.example.work;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private FragmentManager fm = getSupportFragmentManager();
-    private LinearLayout linearLayout1, linearLayout2, linearLayout3, linearLayout4;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Fragment fragment1,fragment2,fragment3,fragment4;
     private ImageView imageView1,imageView2,imageView3,imageView4;
-//设置click监听，并且获取一个事务管理器用于后续添加fragment
-    private Fragment chat = new ChatFragment();
-    private Fragment people = new PeopleFragment();
-    private Fragment find = new FindFragment();
-    private Fragment personal = new PersonalFragment();
+    private FragmentManager manager;
+    private FragmentTransaction transaction;
+
+    private LinearLayout linearLayout1,linearLayout2,linearLayout3,linearLayout4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,25 +29,85 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         linearLayout2=findViewById(R.id.layout2);
         linearLayout3=findViewById(R.id.layout3);
         linearLayout4=findViewById(R.id.layout4);
-        initFragment();
+
+        fragment1=new ChatFragment();
+        fragment2=new PeopleFragment();
+        fragment3=new FindFragment();
+        fragment4=new PersonalFragment();
+
+        manager=getSupportFragmentManager();
         initImageView();
-        showFragment(0);
+        initial();
+
+        hidden();
 
         linearLayout1.setOnClickListener(this);
         linearLayout2.setOnClickListener(this);
         linearLayout3.setOnClickListener(this);
         linearLayout4.setOnClickListener(this);
+
     }
 
-    private void initFragment() {
-        FragmentTransaction transaction = fm.beginTransaction();
-        transaction.add(R.id.layout1, chat);
-        transaction.add(R.id.layout2, people);
-        transaction.add(R.id.layout3, find);
-        transaction.add(R.id.layout4, personal);
+    private void initial() {
+        transaction=manager.beginTransaction()
+                .add(R.id.frameLayout,fragment1)
+                .add(R.id.frameLayout,fragment2)
+                .add(R.id.frameLayout,fragment3)
+                .add(R.id.frameLayout,fragment4);
         transaction.commit();
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.layout1:select(1);
+                break;
+            case R.id.layout2:select(2);
+                break;
+            case R.id.layout3:select(3);
+                break;
+            case R.id.layout4:select(4);
+                break;
+        };
+    }
+
+    private void select( int i) {
+        TextView textView = findViewById(R.id.textView);
+        hidden();
+        resetImg();
+        switch (i){
+            case 1:showfragment(fragment1);
+                imageView1.setImageResource(R.drawable.weixinthis);
+                textView.setText(R.string.vx);
+                break;
+            case 2:showfragment(fragment2);
+                imageView2.setImageResource(R.drawable.contactthis);
+                textView.setText(R.string.contact);
+                break;
+            case 3:showfragment(fragment3);
+                imageView3.setImageResource(R.drawable.findthis);
+                textView.setText(R.string.find);
+                break;
+            case 4:showfragment(fragment4);
+                imageView4.setImageResource(R.drawable.myselfthis);
+                textView.setText(R.string.myself);
+                break;
+        }
+    }
+
+    private void showfragment(Fragment fragment) {
+        transaction.show(fragment);
+    }
+
+    private void hidden() {
+        transaction=manager.beginTransaction()
+                .hide(fragment1)
+                .hide(fragment2)
+                .hide(fragment3)
+                .hide(fragment4);
+        transaction.commit();
+
+    }
     private void initImageView() {
         imageView1 = findViewById(R.id.imageView1);
         imageView2 = findViewById(R.id.imageView2);
@@ -60,38 +120,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         linearLayout4 = findViewById(R.id.layout4);
     }
 
-    private void hideFragment(FragmentTransaction transaction) {
-        transaction.hide(chat);
-        transaction.hide(people);
-        transaction.hide(find);
-        transaction.hide(personal);
-    }
-
-    @Override
-    public void onClick(View view) {
-        FragmentTransaction fragmentTransaction = fm.beginTransaction();
-        //hideFragment(fragmentTransaction);
-        resetImg();
-        switch (view.getId()) {
-            case R.id.layout1:
-                showFragment(0);
-                Log.d("xr","transaction.show(chat).....");
-                break;
-            case R.id.layout2:
-                showFragment(1);
-                Log.d("xr","transaction.show(people).....");
-                break;
-            case R.id.layout3:
-                showFragment(2);
-                break;
-            case R.id.layout4:
-                showFragment(3);
-                break;
-            default:
-                break;
-        }
-    }
-
     private void resetImg() {    //调用灰色的图片，以让点击过后的图片回复原色
         imageView1.setImageResource(R.drawable.weixin);
         imageView2.setImageResource(R.drawable.contact);
@@ -100,30 +128,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    private void showFragment(int i) {    //控制图片颜色的变换，其意义是点击一个图片之后该图片就会变亮
-        FragmentTransaction transaction = fm.beginTransaction();
-        hideFragment(transaction);
-        switch (i) {
-            case 0:
-                transaction.show(chat);
-                imageView1.setImageResource(R.drawable.weixinthis);
-                Log.d("xr","transaction.show(chat).....");
-                break;
-            case 1:
-                transaction.show(people);
-                imageView2.setImageResource(R.drawable.contactthis);
-                break;
-            case 2:
-                transaction.show(find);
-                imageView3.setImageResource(R.drawable.findthis);
-                break;
-            case 3:
-                transaction.show(personal);
-                imageView4.setImageResource(R.drawable.myselfthis);
-                break;
-            default:
-                break;
-        }
-        transaction.commit();
-    }
+
 }
